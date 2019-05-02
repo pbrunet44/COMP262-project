077e55091c3d714e397d69405ca9c77a40e9154e
pbrunet44 <pbrunet44@gmail.com>
2019-05-02 13:14:47 -0700

    Added OpenMP untilization

--- factorize.c
+++ factorize.c
@@ -6,7 +6,8 @@
 **/
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <omp.h>
+#include <stdbool.h>
 int main(int argc, char *argv[])
 {
 	if(argc != 2)
@@ -14,7 +15,36 @@ int main(int argc, char *argv[])
 		printf("Usage: ./factorize <large multiple of two primes>\n");
 		return(0); //Return if no parameters or too many
 	}
-	long multiple = strtol(argv[1], NULL, 10);
-	
 
+	long parameter = strtol(argv[1], NULL, 10); //long to hold parameter
+	int nthreads;
+	int tid; //ints to hold number of threads and thread IDs
+	long prime1;
+	long prime2; //longs to hold possible primes
+	bool flag = false; //Checks if a result has been found
+	#pragma omp parallel shared(nthreads,parameter, flag) private(tid, prime1, prime2) //Split into multiple threads
+	{
+		tid = omp_get_thread_num();
+		printf("Thread number %d checking in!\n", tid);
+		if(tid == 0)
+		{
+			nthreads = omp_get_num_threads();
+		}
+		while(!flag)
+		{
+			prime1 = rand() % 25;
+			do
+			{
+				prime2 = rand() % 25;
+			} while(prime1 == prime2);
+			printf("Checking %ld and %ld on thread %d\n", prime1, prime2, tid);
+			if(prime1 * prime2 == parameter)
+			{
+				flag = true;
+				printf("ANSWER FOUND! %ld and %ld are the factors of %ld\n", prime1, prime2, parameter);
+			}
+		}
+
+	} //All threads join
+	printf("All threads successfully joined.\nThere were %d threads.\n", nthreads);
 }
